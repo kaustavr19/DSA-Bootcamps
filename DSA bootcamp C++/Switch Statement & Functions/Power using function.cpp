13a48cdbd8ba9382befb026412bcef79795b5bef
kaustavr19 <kaustavr25@gmail.com>
2021-12-21 14:12:25 +0530

    calculator switch power function

---
+++ DSA bootcamp C++/Switch Statement & Functions/Power using function.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int power(){
+    int a,b;
+    cout<<"Enter the numbers: "<<endl;
+    cin >> a >> b;
+
+    int ans = 1;
+
+    for(int i = 1; i <= b; i++){
+        ans = ans*a;
+    }
+
+    return ans;
+}
+
+int main(){
+    int ans = power();
+    cout << " answer is " << ans << endl;
+
+    return 0;
+}
