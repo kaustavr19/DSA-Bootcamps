259d99fc954c6d215c2a02ce1bbe5bfcab77e99d
Kaustav Roy <kaustavr25@gmail.com>
2022-02-16 12:56:53 +0530

    scope of array

---
+++ DSA bootcamp C++/Intro to Array/arrayScope.cpp
@@ -0,0 +1,33 @@
+#include<iostream>
+using namespace std;
+
+void update(int arr[], int size){
+
+    cout << "Inside the function" << endl;
+
+    //updating array's first element
+    arr[0] = 120;
+
+    //printing the array
+    for(int i=0; i<3; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    cout << "Going back to main function" << endl;
+}
+
+int main(){
+    int arr[3] = {1,2,3};
+
+    update(arr, 3);
+
+    cout << "Printing in main function: " << endl;
+    //printing the array
+    for(int i=0; i<3; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
